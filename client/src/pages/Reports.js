6a109a60fe25daee26add217f0fcc67faf218ac4
EllieThor <elliethor18@gmail.com>
2021-05-31 15:43:35 +0300

    add myChart to reports

--- client/src/pages/Reports.js
+++ client/src/pages/Reports.js
@@ -5,11 +5,11 @@ import { connect } from "react-redux";
 import * as Api from "../Api/apiCalls";
 import { Route, Link } from "react-router-dom";
 import Chart from "chart.js/auto";
+import ReportsCOMP from "../components/reportsComponent";
 
 class Reports extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  componentDidMount() {
+    this.getGraph();
   }
   // patterns OBJ
   inputsObj = {
@@ -19,10 +19,42 @@ class Reports extends Component {
     this.inputsObj[e.target.id] = e.target.value;
     console.log("new Input To inputsObj :", e.target.id, "value: ", e.target.value);
   };
+  getGraph = () => {
+    console.log("this.props.vacationsName: ", this.props.vacationsNames);
+    let ctx = document.getElementById("myChart").getContext("2d");
+    let myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: this.props.vacationsNames,
+        datasets: [
+          {
+            // TODO: מה זה הלייבל
+            label: "# of Votes",
+            // data: [12, 19, 3, 5, 2, 3],
+            data: this.props.numberOfStars,
+            backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
+            borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+            borderWidth: 1,
+          },
+        ],
+      },
+      // options: {
+      //   scales: {
+      //     y: {
+      //       beginAtZero: true,
+      //     },
+      //   },
+      // },
+    });
+  };
   render() {
     return (
       <div className="container">
-        <div>Reports</div>
+        {/* <div id="graphDIV" className="graph">
+          <canvas id="myChart" className="graph"></canvas>
+        </div> */}
+        <ReportsCOMP />
       </div>
     );
   }
