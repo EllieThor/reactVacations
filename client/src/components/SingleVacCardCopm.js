dd3bff22cfdf1b4479b02b668e575443ee0c58ea
EllieThor <elliethor18@gmail.com>
2021-07-23 15:19:51 +0300

    style single card

--- client/src/components/SingleVacCardCopm.js
+++ client/src/components/SingleVacCardCopm.js
@@ -6,37 +6,48 @@ import EditIcons from "./IconsEditComp";
 const SingleVacationCard = (props) => {
   let vacationCard = props.vacations.map((vacation, i) => {
     let img = "http://localhost:5004/" + vacation.ImageName;
+    let starImg = "http://localhost:5004/star4.png";
 
     return (
       <div key={i} className="col-sm-12 col-md-6 col-xl-3 p-3">
         <div className="card single-card">
           <img className="card-img-top productImg" src={img} alt={vacation.Destination} />
+          <div class="card-header text-center">
+            <h5 className="card-title">{vacation.Destination}</h5>
+          </div>
           <div className="card-body productCard d-grid gap-2">
             <div className="row">
-              <div className="col-7">
-                <h5 className="card-title">{vacation.Destination}</h5>
-              </div>
-              {/* TODO: fix style number of start */}
-              <div className="col-2">
-                <div>{vacation.follows === undefined ? "" : vacation.follows.length}</div>
-              </div>
-              <div className="col-3">{props.user.Role === 1 ? <EditIcons vacationToEdit={vacation} deleteVacationFromDB={props.deleteVacationFromDB} openModalEdit={props.openModalEdit} /> : <StarsIcons vacation={vacation} userID={props.user.ID} insertStarToDB={props.insertStarToDB} deleteStarFromDB={props.deleteStarFromDB} />}</div>
-            </div>
-            <div className="row">
-              <div>Description</div>
               <p className="card-text cardDescription">{vacation.Description}</p>
-            </div>
-            <div className="row">
-              <div className="col-3">Price</div>
-              <div className="col-9">
-                <p className="card-text">{vacation.Price} &#36;</p>
-              </div>
             </div>
             <div className="row">
               <div className="col-3">Dates</div>
               <div className="col-9">
                 <div className="card-text">
                   {vacation.StartDate.slice(0, 10).replaceAll("-", "/").split("/").reverse().join("/")}-{vacation.EndDate === null ? "" : vacation.StartDate.slice(0, 10).replaceAll("-", "/").split("/").reverse().join("/")}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer text-muted">
+            <div className="row">
+              <div className="col-6">
+                <div className="row">
+                  <div className="col-5">Price</div>
+                  <div className="col-7">{vacation.Price} &#36;</div>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="row">
+                  <div className="col-6">
+                    <div className="numOfStars">
+                      <abbr title={vacation.follows.length <= 0 ? "There are no stars for this vacation" : `There are ${vacation.follows.length} stars to this vacation`}>
+                        <i className="far fa-star" />
+                      </abbr>
+                      &nbsp;&nbsp;
+                      {vacation.follows === undefined ? "" : vacation.follows.length}
+                    </div>
+                  </div>
+                  <div className="col-6">{props.user.Role === 1 ? <EditIcons vacationToEdit={vacation} deleteVacationFromDB={props.deleteVacationFromDB} openModalEdit={props.openModalEdit} /> : <StarsIcons vacation={vacation} userID={props.user.ID} insertStarToDB={props.insertStarToDB} deleteStarFromDB={props.deleteStarFromDB} />}</div>
                 </div>
               </div>
             </div>
