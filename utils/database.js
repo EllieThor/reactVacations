03dc2eb1529a69b0dba8bacddbaaf11822116411
EllieThor <elliethor18@gmail.com>
2021-08-06 21:29:21 +0300

    delete img upload react upload to cloudinary

--- utils/database.js
+++ utils/database.js
@@ -9,4 +9,3 @@ const sequelize = new Sequelize("heroku_0a172e6deb10439", "bcc6fed49c3d6d", "8c3
 module.exports = sequelize;
 
 // mysql://bcc6fed49c3d6d:8c3e828c@eu-cdbr-west-01.cleardb.com/heroku_0a172e6deb10439?reconnect=true
-// mysql://bcc6fed49c3d6d:8c3e828c@eu-cdbr-west-01.cleardb.com/heroku_0a172e6deb10439?reconnect=true
