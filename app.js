799866409ab986d68e792ba637eb60c1d0e607f7
EllieThor <elliethor18@gmail.com>
2021-08-08 15:11:37 +0300

    socket stack try change to io listen

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
 app.use(bodyParser.json());
 
 var corsOptions = {
-  origin: "https://vacations-stars.netlify.app/",
+  origin: "https://vacations-stars.netlify.app",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
 };
