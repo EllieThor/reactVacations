063e79d4850e2cd4e9cca909e95bc049eb58c458
EllieThor <elliethor18@gmail.com>
2021-08-08 19:36:50 +0300

    socket stack try change to io listen

--- app.js
+++ app.js
@@ -18,19 +18,6 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  // Add this
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, OPTIONS");
-    res.header("Access-Control-Max-Age", 120);
-    return res.status(200).json({});
-  }
-
-  next();
-});
 // socket.io plugins
 const http = require("http");
 const server = http.createServer(app);
@@ -41,6 +28,7 @@ const io = socketIO(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
+  transports: ["websocket"],
 });
 
 // const io = socketIO(server, {
