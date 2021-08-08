861a72b096e0291fa5e4ea0b71ec70981f57f9c1
EllieThor <elliethor18@gmail.com>
2021-08-08 14:12:22 +0300

    socket stack try change to io listen

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const io = socketIO(server, {
     handlePreflightRequest: (req, res) => {
       res.writeHead(200, {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Methods": "POST",
         "Access-Control-Allow-Headers": "my-custom-header",
         "Access-Control-Allow-Credentials": false,
       });
