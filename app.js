fee5a7179fd7f7f2f5cb0d4358620af793163bbc
EllieThor <elliethor18@gmail.com>
2021-08-09 20:36:38 +0300

    socket stack try change to io listen

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ io.on("connection", (socket) => {
   });
 });
 // server.listen();
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 14683);
 
 app.listen(app.get("port"), function () {
   console.log("Node app is running on port", app.get("port"));
