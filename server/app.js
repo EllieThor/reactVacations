0493904ae960898247fcc70a919469567dae4709
EllieThor <74139385+EllieThor@users.noreply.github.com>
2023-05-06 22:05:47 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -46,30 +46,30 @@ app.use("/vacations", VacationsRoute);
 
 // IMAGE UPLOADING
 // specify the folder
-//app.use(express.static(path.join(__dirname, "uploads")));
-//app.use(function (req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "https://vacations-stars.netlify.app");
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //next();
-//});
+app.use(express.static(path.join(__dirname, "uploads")));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-//var storage = multer.diskStorage({
-  //destination: function (req, file, cb) {
-    //cb(null, "./uploads/");
-  //},
-  //filename: function (req, file, cb) {
-    //let imgEnd = file.originalname.split(".");
-    //imgEnd = imgEnd[imgEnd.length - 1];
-    //cb(null, file.originalname);
-  //},
-//});
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    let imgEnd = file.originalname.split(".");
+    imgEnd = imgEnd[imgEnd.length - 1];
+    cb(null, file.originalname);
+  },
+});
 
-//var upload = multer({ storage: storage });
+var upload = multer({ storage: storage });
 
-//app.post("/upload", upload.array("uploads[]", 12), function (req, res) {
- // imgEnd = "";
- // res.send(req.files);
-//});
+app.post("/upload", upload.array("uploads[]", 12), function (req, res) {
+  imgEnd = "";
+  res.send(req.files);
+});
 
 sequelize
   .sync()
