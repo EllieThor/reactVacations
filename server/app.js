850db812b03a3bdf4ababe9514964b52abecc342
EllieThor <74139385+EllieThor@users.noreply.github.com>
2023-05-14 12:54:59 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 
 var corsOptions = {
  origin: "https://vacations-stars.netlify.app",
- // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
