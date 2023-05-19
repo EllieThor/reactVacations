1cebcd0bf51208d1626221f64fbdf9ab7c45b1eb
EllieThor <74139385+EllieThor@users.noreply.github.com>
2023-05-19 12:35:27 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -32,7 +32,9 @@ app.use(
 app.use(bodyParser.json());
 
 var corsOptions = {
- origin: "https://vacations-stars.netlify.app",
+// origin: "https://vacations-stars.netlify.app",
+   origin: "*",
+
  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -44,32 +46,32 @@ app.use("/users", UsersRoute);
 const VacationsRoute = require("./routs/vacationRoute");
 app.use("/vacations", VacationsRoute);
 
-// IMAGE UPLOADING
-// specify the folder
-app.use(express.static(path.join(__dirname, "uploads")));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// // IMAGE UPLOADING
+// // specify the folder
+// app.use(express.static(path.join(__dirname, "uploads")));
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    let imgEnd = file.originalname.split(".");
-    imgEnd = imgEnd[imgEnd.length - 1];
-    cb(null, file.originalname);
-  },
-});
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     let imgEnd = file.originalname.split(".");
+//     imgEnd = imgEnd[imgEnd.length - 1];
+//     cb(null, file.originalname);
+//   },
+// });
 
-var upload = multer({ storage: storage });
+// var upload = multer({ storage: storage });
 
-app.post("/upload", upload.array("uploads[]", 12), function (req, res) {
-  imgEnd = "";
-  res.send(req.files);
-});
+// app.post("/upload", upload.array("uploads[]", 12), function (req, res) {
+//   imgEnd = "";
+//   res.send(req.files);
+// });
 
 sequelize
   .sync()
